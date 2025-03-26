69ecec4cad7f751d5df4ac1167cb6b99055c31e1
Haydée Laiz <haydeelgp@gmail.com>
2025-03-26 15:08:52 -0300

    Adicionando requirements.txt e configurações do cors

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, jsonify
-
 import sqlite3
+
+from flask import Flask, request, jsonify
+import flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route("/pague")
